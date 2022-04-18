b440a80ec6fd85049de38830cb256adb93306191
Raymond Broome <rbroome@gmail.com>
2022-04-17 22:50:51 -0500

    Add additional fields to signup form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import DealsForYou from "../components/FeaturedHomePage/DealsForYou/DealsForYou";
 import Footer from "../components/Footer/Footer";
 import GoodDeals from "../components/FeaturedHomePage/GoodDeals/GoodDeals";
-import Header from "../components/Header/Header";
 import Topdeals from "../components/FeaturedHomePage/TopDeals/Topdeals";
 import HealthFeaturedProducts from "../components/FeaturedHomePage/HealthProducts/HealthFeaturedProducts";
 
